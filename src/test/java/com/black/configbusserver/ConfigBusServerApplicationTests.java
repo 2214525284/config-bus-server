f5bccf2f71f25b6769678989dd6d2eab873cdee3
lijiahao <hao_0414@163.com>
2020-07-08 11:28:10 +0800

    test

---
+++ src/test/java/com/black/configbusserver/ConfigBusServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.black.configbusserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigBusServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
